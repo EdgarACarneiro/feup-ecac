13a10aee95428999c6af78a8536e30bd6e06ea80
EdgarACarneiro <edgarcarneiro1306@gmail.com>
2019-09-17 15:25:45 +0100

    Setting base workflow

---
+++ project-competition/utils.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import os
+
+
+def read_csv_to_df(filename):
+    '''Read a csv to a pandas dataframe'''
+    return pd.read_csv(filename)
+
+
+def write_df_to_csv(df, directory, filename):
+    '''Write the content of a dataframe to a csv file'''
+    csv = df.to_csv(index=False)
+
+    os.makedirs(directory, exist_ok=True)
+    output_path = '%s/%s' % (directory, filename)
+
+    with open(output_path, 'w') as fd:
+        fd.write(csv)
