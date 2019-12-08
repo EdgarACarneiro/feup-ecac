fffe89d8278825d0056b7d0568eea3b04b3a2fdd
cyrilico <jpdamas15@gmail.com>
2019-12-08 12:10:48 +0000

    Calculate marginal gain and greedy heuristic

--- research-project/lookout.py
+++ research-project/lookout.py
@@ -2,13 +2,28 @@ from sklearn.ensemble import IsolationForest as IF
 import pandas as pd
 from itertools import combinations as ncr
 import numpy as np
+from timeit import default_timer as timer
+
+#Get row indices of given feature pairs
+def get_row_indices(S, feature_pairs):
+    return list(map(lambda elem: feature_pairs.index(elem), S))
+
+#Calculate total incrimination score of a given subset of plots (feature pairs) s
+def f(S, outlier_scores):
+    #Base case
+    if not len(S):
+        return 0
+
+    scores_S = outlier_scores[S, :] #Only keep rows of plots being analyzed
+    return sum(np.max(scores_S,axis=0)) #Max score on each column (best plot for each outlier) and respective sum of all values
 
 #Number of plots to choose
 BUDGET = 3
 
+time = -timer()
 #Load dataset
-full_df = pd.read_csv("HTRU_2.csv", nrows=100) #Faster and easier alternative to test (worse results, of course)
-#full_df = pd.read_csv("HTRU_2.csv")
+#full_df = pd.read_csv("HTRU_2.csv", nrows=1000) #Faster and easier alternative to test (worse results, of course)
+full_df = pd.read_csv("HTRU_2.csv")
 
 #Isolate outliers
 outlier_df = full_df.loc[full_df['Class'] == 1]
@@ -29,10 +44,20 @@ for feature_pair in feature_pairs:
     scores = np.array([classifier.decision_function(outlier_df[list(feature_pair)]).tolist()]) if scores is None \
                 else np.append(scores, [classifier.decision_function(outlier_df[list(feature_pair)]).tolist()], axis=0)
 
-# clf = IF()
-# clf.fit(df)
-# #print(clf.decision_function(df))
-# y_pred_train = clf.predict(df)
-# for idx, comp in list(enumerate(zip(y_pred_train, real_outliers)))[:100]:
-#     if comp[0] != comp[1]:
-#         print("{} different: expected {}, but got {}".format(idx, comp[1], comp[0]))
+#Plot selection using greedy heuristic approach (see paper for proof of near optimality)
+S = [] #Final plot selection
+while BUDGET > 0:
+    candidate_pairs = list(set(feature_pairs) - set(S)) #Only pairs that have not been selected already
+    candidate_pairs_marginal_gains = []
+    for candidate_pair in candidate_pairs:
+        candidate_pairs_marginal_gains.append(\
+                        f(get_row_indices(S+[candidate_pair], feature_pairs), scores) - f(get_row_indices(S, feature_pairs), scores)) #Marginal gain of current feature pair
+    
+
+    S.append(candidate_pairs[candidate_pairs_marginal_gains.index(max(candidate_pairs_marginal_gains))]) #Get max marginal gain, its index and retrieve respective feature pair
+    BUDGET = BUDGET - 1
+
+time = time + timer()
+
+print("Final selection: {}".format(S))
+print("Execution time: {0:.2f}s".format(time))
